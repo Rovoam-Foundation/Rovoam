fad0673f07696ba3bb00e3b5c5a3e241f2b95e73
Kisel13 <aleksei201122@gmail.com>
2025-08-03 13:23:51 +0300

    add flake.nix removed snake.json and venv

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Dev-shell flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }@inputs: 
+  let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    python = pkgs.python3;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [
+        (python.withPackages(pypkgs: with pypkgs; [
+          rich
+	  openai
+        ]))
+      ];
+    };
+  };
+}
